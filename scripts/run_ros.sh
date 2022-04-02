1075147638844a02f49ef3ef576b4023bef7ef4e
Petrik van der Velde <petrikvandervelde@gmail.com>
2022-04-02 22:18:02 +1300

    Actually launch the right launch file

--- scripts/run_ros.sh
+++ scripts/run_ros.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash -c "source /opt/ros/noetic/setup.bash && source devel/setup.bash && roslaunch runslam slam.launch"
+bash -c "source /opt/ros/noetic/setup.bash && source /home/pi/scuttle_ws/devel/setup.bash && roslaunch scuttle_bringup scuttle_bringup_daemon.launch"
