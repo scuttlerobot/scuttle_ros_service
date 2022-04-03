e8bcabfb61294af507712abf6b9e4e74e29447bf
Petrik van der Velde <petrikvandervelde@gmail.com>
2022-04-03 15:11:37 +1200

    Removing the lonely double quote that shouldn't be there

--- scripts/run_ros.sh
+++ scripts/run_ros.sh
@@ -7,4 +7,4 @@ source /opt/ros/noetic/setup.bash
 source /home/pi/scuttle_ws/devel/setup.bash
 
 # Start ROS
-roslaunch scuttle_bringup scuttle_bringup_daemon.launch"
+roslaunch scuttle_bringup scuttle_bringup_daemon.launch
